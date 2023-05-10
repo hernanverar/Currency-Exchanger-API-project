74044c614dbdf3a807bb3a27bb47d47984b043ba
hernan verar <hernanverar@gmail.com>
2023-05-09 21:23:19 -0700

    This adds a function error accesing data also resets buttons to 0 everytime user is done

--- src/index.js
+++ src/index.js
@@ -1,33 +1,60 @@
 import "./css/styles.css";
 import ExchangeService from "./exchange.js";
 
-//Business Logic
+// //Business Logic
 
 const convertCurrency = (exchangeCurrencyRate, dollarAmmount) => {
-  return (exchangeCurrencyRate*dollarAmmount).toFixed(0);
+  return (exchangeCurrencyRate * dollarAmmount).toFixed(0);
 };
+
+function errorMessage(error) {
+  document.querySelector("#error-message").setAttribute("class", "error");
+  document.querySelector(
+    "#error-message"
+  ).innerText = `There was an error accessing the exchange data: ${error.message}`;
+}
 
 async function getExchange(exchangeCurrency, dollarAmmount) {
   const rates = await ExchangeService.getRate();
-  if(!rates.conversion_rates[exchangeCurrency]){
+  if (!rates.conversion_rates[exchangeCurrency]) {
+    throw new Error("Exchange rate not found");
   }
-  // console.log(rates.conversion_rates[exchangeCurrency]);
-  return convertCurrency(rates.conversion_rates[exchangeCurrency], dollarAmmount);
+  return convertCurrency(
+    rates.conversion_rates[exchangeCurrency],
+    dollarAmmount
+  );
 }
 
-//UI logic
+// UI logic
 
 async function handleFormSubmission(e) {
   e.preventDefault();
 
   const amountToConvert = document.getElementById("usDollar").value;
   const currencyCode = document.getElementById("exchangeToCurrency").value;
-  const convertedCurrencyAmount = await getExchange(currencyCode,amountToConvert);
 
-  // console.log("converted currency amount", convertedCurrencyAmount);
+  // Reset elements
+  document.querySelector("#displayExchange").innerText = "";
+  document.querySelector("#error-message").innerText = "";
+  document.querySelector("#displayExchange").setAttribute("class", "hidden");
+  document.querySelector("#error-message").setAttribute("class", "hidden");
 
-  document.querySelector("#displayExchange").innerText = `${amountToConvert} USD is equal to ${convertedCurrencyAmount} ${currencyCode}`;
-  document.querySelector("#displayExchange").removeAttribute("class");
+  try {
+    const convertedCurrencyAmount = await getExchange(
+      currencyCode,
+      amountToConvert
+    );
+    document.querySelector(
+      "#displayExchange"
+    ).innerText = `${amountToConvert} USD is equal to ${convertedCurrencyAmount} ${currencyCode}`;
+    document.querySelector("#displayExchange").removeAttribute("class");
+  } catch (error) {
+    errorMessage(error);
+  }
+
+  // Reset input fields
+  document.getElementById("usDollar").value = "";
+  document.getElementById("exchangeToCurrency").value = "";
 }
 
 window.addEventListener("load", function () {
